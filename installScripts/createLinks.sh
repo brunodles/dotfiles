ce6ba859ba59f0f280d12f2df80cf8af95d4f7d6
Bruno de Lima <bruno.lima@uber.com>
2024-10-22 14:24:10 -0300

    Update installScript to backup original .zshrc before creating a link into my ocnfig

--- installScripts/createLinks.sh
+++ installScripts/createLinks.sh
@@ -4,4 +4,8 @@
 # With this kind of link we can keep all the configurations under the same folder/repo.
 
 # Link ~/.zshrc
+mkdir -p ~/.config/backup/
+cp ~/.zshrc ~/.config/backup/
+rm ~/.zshrc
 ln -s ~/.config/zsh/zshrc ~/.zshrc
+
