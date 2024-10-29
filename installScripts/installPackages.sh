75be9692ffcc12f58ccd477adba75563ae879456
Bruno de Lima <bruno.lima@uber.com>
2024-10-29 11:28:39 -0300

    Split installPackages script

--- installScripts/installPackages.sh
+++ installScripts/installPackages.sh
@@ -2,136 +2,34 @@
 # Install Packages for having this running
 # Author: Bruno de Lima <github.com/brunodles>
 
-# These variables will be used to accumulate the contents
-declare -a _linux_shared=()
-declare -a _linux_servers=()
-declare -a _linux_ui=()
-
-declare -a _brew=()
-declare -a _brew_cask=()
-
-
-shared() {
-  _linux_shared+=($@)  
-}
-server() {
-  _linux_servers+=($@)
-}
-lui() {
-  _linux_ui+=($@)
-}
-
-brew() {
-  _brew+=($@)
-}
-cask() {
-  _brew_cask+=($@)
-}
-
-
-where() {
-  echo $(command -v $1)
-}
-linuxUpdatePackages() {
-  echo "LinuxUpdatePackages"
-  if [ ! -z "$(where pacman)" ]; then
-    sudo pacman -Syu
-  elif [ ! -z "$(where apt)" ]; then
-    sudo apt update
-  else
-    echo $@
-  fi
-}
-
-linuxInstall() {
-  echo "LinuxInstall $@"
-  if [ ! -z "$(where pacman)" ]; then
-    sudo pacman -S $@
-  elif [ ! -z "$(where apt)" ]; then
-    sudo apt install -y $@
-  else
-    echo $@
-  fi
-}
+source installUtils.sh
 
 # Linux
-shared base-devel build-essential
-shared vim curl git tmux zsh
-shared neovim
-## docker requires a custom instalation script
-#shared docker docker-compose-plugin
-#shared docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-lui firefox alacritty feh compton i3 i3blocks
-#servers
+  shared base-devel build-essential
+  shared vim curl git tmux zsh
+  shared neovim
+
+## Docker - requires a custom instalation script
+  #shared docker docker-compose-plugin
+  #shared docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+## UI customization / Desktop Environment
+  lui firefox alacritty feh compton i3 i3blocks
+
+## Servers 
+  #servers ?
+
 ## Android
-shared pidcat-git
-lui scrcpy
-#server ?
+  shared pidcat-git
+  lui scrcpy
 
 # Mac
-brew vim curl git tmux zsh
-brew neovim
-brew docker docker-compose-plugin
-brew scrcpy pidcat
-cask alacritty
-
-declare uname=$(uname)
-declare -a tools=()
-tools+=()
-case $uname in
-  "Linux")
-    tools+=(${_linux_shared[@]})
-    if [ ! -z "$XDG_CURRENT_DESKTOP" ]; then
-      tools+=(${_linux_ui[@]})
-    else
-      tools+=(${_linux_server[@]})
-    fi
-    if [ -z "$(where docker)" ] || [ -z "$(where docker-compose)" ]; then
-      tools+=(docker docker-compose)
-    fi
-    ;;
-  "Darwin")
-    tools+=(${_brew[@]})
-    tools+=(${_brew_cask[@]})
-    ;;
-esac
-if [ ! -e "$HOME/.oh-my-zsh" ]; then
-  tools+=("oh-my-zsh")
-fi
-
-echo "This script will install: \"${tools[@]}\"."
-read -p ":: Continue install? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+  brew vim curl git tmux zsh
+  brew neovim
+#  brew docker docker-compose-plugin
+  brew scrcpy pidcat
+#  cask alacritty
 
 
-case $uname in
-  "Linux")
-    linuxUpdatePackages
-    linuxInstall ${_linux_shared[@]}
-    if [ ! -z "$XDG_CURRENT_DESKTOP" ]; then
-      linuxInstall ${_linux_ui[@]}
-    else
-      linuxInstall ${_linux_server[@]}
-    fi
-    ;;
-  "Darwin")
-    sudo brew install ${_brew[@]}
-    sudo brew install --cask install ${_brew_cask[@]}
-    ;;
-esac
-#install ${tools[@]}
+executeInstall
 
-# Zsh
-if [[ " ${tools[*]} " == *" oh-my-zsh "* ]]; then
-  echo install oh-my-zsh
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
-
-# Docker, DockerCompose
-if [[ " ${tools[*]} " == *" docker "* ]]; then
-  echo Installl docker
-  curl -o /tmp/docker-desktop-amd64.deb https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb
-  sudo apt-get update
-  sudo apt-get install /tmp/docker-desktop-amd64.deb
-  #rm -f /tmp/docker-desktop-amd64.deb
-fi
- 
---
+++ installScripts/installUtils.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+# Utility for installing packages in linux and ubuntu.
+# The functions in this document will be used by another script.
+# Author: Bruno de Lima <github.com/brunodles>
+
+# These variables will be used to accumulate the contents
+declare -a _linux_shared=()
+declare -a _linux_servers=()
+declare -a _linux_ui=()
+
+declare -a _brew=()
+declare -a _brew_cask=()
+
+
+shared() {
+  _linux_shared+=($@)  
+}
+server() {
+  _linux_servers+=($@)
+}
+lui() {
+  _linux_ui+=($@)
+}
+
+brew() {
+  _brew+=($@)
+}
+cask() {
+  _brew_cask+=($@)
+}
+
+
+where() {
+  echo $(command -v $1)
+}
+linuxUpdatePackages() {
+  echo "LinuxUpdatePackages"
+  if [ ! -z "$(where pacman)" ]; then
+    sudo pacman -Syu
+  elif [ ! -z "$(where apt)" ]; then
+    sudo apt update
+  else
+    echo $@
+  fi
+}
+
+linuxInstall() {
+  echo "LinuxInstall $@"
+  if [ ! -z "$(where pacman)" ]; then
+    sudo pacman -S $@
+  elif [ ! -z "$(where apt)" ]; then
+    sudo apt install -y $@
+  else
+    echo $@
+  fi
+}
+
+
+executeInstall() {
+	declare uname=$(uname)
+	declare -a tools=()
+	tools+=()
+	case "$uname" in
+		"Linux")
+			tools+=(${_linux_shared[@]})
+			if [ ! -z "$XDG_CURRENT_DESKTOP" ]; then
+				tools+=(${_linux_ui[@]})
+			else
+				tools+=(${_linux_server[@]})
+			fi
+			if [ -z "$(where docker)" ] || [ -z "$(where docker-compose)" ]; then
+				tools+=(docker docker-compose)
+			fi
+			;;
+		"Darwin")
+			tools+=(${_brew[@]})
+			tools+=(${_brew_cask[@]})
+			;;
+	esac
+	if [ ! -e "$HOME/.oh-my-zsh" ]; then
+		tools+=("oh-my-zsh")
+	fi
+
+	echo "This script will install: \"${tools[@]}\"."
+	read -p ":: Continue install? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
+
+	case "$uname" in
+		"Linux")
+			linuxUpdatePackages
+			linuxInstall ${_linux_shared[@]}
+			if [ ! -z "$XDG_CURRENT_DESKTOP" ]; then
+				linuxInstall ${_linux_ui[@]}
+			else
+				linuxInstall ${_linux_server[@]}
+			fi
+			;;
+		"Darwin")
+			if [[ ! -z "${_brew[@]}" ]]; then
+			  local installCommand="brew install ${_brew[@]}"
+			  echo "$installCommand"
+			  sh -c "$installCommand"
+			fi
+			if [[ ! -z "${_brew_cask[@]}" ]]; then 
+			  local installCommand="brew install --cask ${_brew_cask[@]}"
+			  echo "$installCommand"
+			  sh -c "$installCommand"
+			fi
+			;;
+	esac
+	#install ${tools[@]}
+
+	# Zsh
+	if [[ " ${tools[*]} " == *" oh-my-zsh "* ]]; then
+		echo install oh-my-zsh
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	fi
+
+	# Docker, DockerCompose
+	if [[ " ${tools[*]} " == *" docker "* ]]; then
+		echo Installl docker
+		curl -o /tmp/docker-desktop-amd64.deb https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb
+		sudo apt-get update
+		sudo apt-get install /tmp/docker-desktop-amd64.deb
+		#rm -f /tmp/docker-desktop-amd64.deb
+	fi
+}
+ 
