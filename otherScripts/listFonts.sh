1a3968b1799d6c7bef3116f3e17f003fa8cae566
Bruno de Lima <dlimaun@gmail.com>
2025-05-04 16:35:17 -0300

    Add parameter for the listFonts script

--- otherScripts/listFonts.sh
+++ otherScripts/listFonts.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # List installed fonts-families and their style
 # Author: Bruno de Lima <github.com/brunodles>
-fc-list --format="%{family[0]} | %{style[0]}\n" | sort | uniq
+
+if [[ -z "$@" ]] ;then
+  fc-list --format="%{family[0]} | %{style[0]}\n" | sort | uniq
+else
+  fc-list --format="%{family[0]} | %{style[0]}\n" | sort | uniq | grep -i "$@"
+fi
 
