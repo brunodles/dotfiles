984415be896f0162c8333266f9866ba7e7532be1
Bruno de Lima <dlimaun@gmail.com>
2025-05-04 16:48:16 -0300

    Update installFonts script

--- installScripts/installFonts.sh
+++ installScripts/installFonts.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# Install font files from multiple sources
 
+# Initialize path and detect font dir
 initialPath=$(pwd)
-
 if [ "$(uname)" == "Darwin" ]; then
   font_dir="$HOME/Library/Fonts"
 else
@@ -9,18 +10,24 @@ else
   mkdir -p $font_dir
 fi
 
+# Install single font from font-awesome
+#  This font file will be used for the i3 Bar
 cd $font_dir
 curl -o FontAwesome-webfont-latest.ttf https://github.com/FortAwesome/Font-Awesome/raw/master/fonts/fontawesome-webfont.ttf
-
 cp "$(dirname "$(readlink -fm "$0")")/fontawesome-webfont.ttf" $font_dir
 
+# Install powerline fonts
 mkdir $initialPath/customization
 cd $initialPath/customization/
 git clone https://github.com/powerline/fonts.git
 cd fonts/
 ./install.sh
 
-fc-cache -f "$font_dir"
+# Install NerdFont from zip files
+cd $initialPath/fonts
+unzip *.zip -d $font_dir
 
+# Refresh font cache
 cd $initialPath
+fc-cache -f "$font_dir"
 
