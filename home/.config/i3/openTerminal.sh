ac92be2a0e9b8e780cbb386b1175b4ed59d7f5e3
Bruno Lima <dlimaun@gmail.com>
2026-01-04 16:53:42 -0300

    Update openTerminal script

--- home/.config/i3/openTerminal.sh
+++ home/.config/i3/openTerminal.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # Open one of my prefered terminals
 
-if [ ! -z "$(command -v alacritty)" ] ; then
-  alacritty
-else
-  i3-sensible-terminal
-fi
+try() {
+  if [ ! -z "$(command -v $1)" ] ; then
+    $1
+    exit 0
+  fi
+}
+
+try ghostty
+try alacritty
+try i3-sensible-terminal
 
