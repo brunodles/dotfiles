fffe4ae59a2ffdb3202922dfc4cb03b35421713e
Bruno de Lima <dlimaun@gmail.com>
2026-06-17 01:25:33 -0300

    Create host/media bootstrap

--- host/media/bootstrap.sh
+++ host/media/bootstrap.sh
@@ -1,10 +1,29 @@
+install_path="$HOME/dotfiles/install"
+install_ubuntu="$install_path/ubuntu"
 
-# bootstrap
-~/dotfiles/bootstrap/ubuntu/bootstrap.sh
-~/dotfiles/bootstrap/ubuntu/snap.sh
-~/dotfiles/bootstrap/ubuntu/docker.sh
-~/dotfiles/bootstrap/ubuntu/filesystem.sh
+# Install common
+$install_ubuntu/bootstrap.sh
+#$install_ubuntu/snap.sh
+$install_ubuntu/filesystem.sh
 
-# linker
-cd home
-~/dotfiles/scripts/linker link
+$install_ubuntu/docker.sh
+
+# other
+$install_path/_samba.post-install.sh
+$install_path/_oh-my-zsh.sh
+$install_path/_tmux.post-install.sh
+
+
+link() {
+  source=$1
+  target=$2
+  rm $target
+  ln -s $source $target
+}
+home_config="$HOME/.config"
+repo_config="$HOME/dotfiles/dotfiles"
+link "$repo_config/.vimrc" "$HOME/.vimrc"
+link "$repo_config/tmux/tmux.conf" "$home_config/tmux/tmux.conf"
+link "$repo_config/zsh" "$home_config/zsh"
+link "$home_config/zsh/zshrc" "$HOME/.zshrc"
+
