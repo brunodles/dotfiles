626e04b33bcc6a0196717c32ed48997bdd2f1060
Bruno de Lima <dlimaun@gmail.com>
2026-06-18 17:57:30 -0300

    Create host vps

---
+++ host/vps/bootstrap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+install_path="$HOME/dotfiles/install"
+install_ubuntu="$install_path/ubuntu"
+
+$install_ubuntu/bootstrap.sh
+$install_ubuntu/ufw.sh
+$install_ubuntu/filesystem.sh
+$install_ubuntu/docker.sh
+$install_path/_oh-my-zsh.sh
+$install_path/_tmux.post-install.sh
+
+# Dockge
+dockge_path="$HOME/dotfiles/host/vps/dockge"
+mkdir -p /opt
+ln -s "$dockge_path/stacks" /opt/stacks
+ln -s "$dockge_path/dockage" /opt/self
+
